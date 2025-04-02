790ad6040ee10878f24053afe1f16336d38f67c1
Pranil Dilip Veer <veerpranil@gmail.com>
2025-04-02 12:21:28 +0530

    Each box onclicking turning Green

--- src/components/Gridbox/GridBox.jsx
+++ src/components/Gridbox/GridBox.jsx
@@ -4,14 +4,20 @@ import "./GridBox.css";
 const GridBox = () => {
   const [grid, setGrid] = useState(Array(9).fill("white"));
 
+  const handleClick = (index) => {
+    const newGrid = [...grid];
+    newGrid[index] = "green";
+    setGrid(newGrid);  };
+
   return (
     <div className="container">
       <div className="grid-box">
-        {grid.map((index) => (
+        {grid.map(( color, index) => (
           <div
             key={index}
+            onClick={() => handleClick(index)}
             className="box"
-            style={{ backgroundColor: "white" }}
+            style={{ backgroundColor: color }}
           ></div>
         ))}
       </div>
